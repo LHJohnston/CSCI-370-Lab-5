482d3f0a62f5abc5d732a93ad54344ababdf918a
Andrew Clark <clarkat@hendrix.edu>
2025-04-02 19:31:56 -0500

    Linked difficulty select with scenes

---
+++ Lab 5/Assets/Scripts/GoToHardLevel.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class GoToHardLevel : MonoBehaviour
+{
+
+    public void ReactToClick() {
+        Debug.Log("I've been clicked");
+        Initiate.Fade("Scene 3", Color.black, 1.0f);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
