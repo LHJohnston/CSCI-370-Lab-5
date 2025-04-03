8eea94bd8ce45c9de8be762a89b8a6af91346eba
Andrew Clark <clarkat@hendrix.edu>
2025-04-02 20:09:39 -0500

    Animated character

--- Lab 5/Assets/Scripts/PlayerMovement.cs
+++ Lab 5/Assets/Scripts/PlayerMovement.cs
@@ -5,25 +5,40 @@ public class PlayerMovement : MonoBehaviour {
     private Rigidbody2D rigidbody2D;
     float horizontal;
     float vertical;
+    private Vector2 input;
     private bool facingLeft = true;
     Animator animator;
+    private Vector2 lastMoveDirection;
     
 
     public float runSpeed = 5f;
     void Start() {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update() {
         float moveX = horizontal = Input.GetAxisRaw("Horizontal");
         float moveY = vertical = Input.GetAxisRaw("Vertical");
+
+        if((moveX == 0 && moveY == 0) && horizontal != 0 || vertical != 0){
+            lastMoveDirection = input;
+        }
         
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        input.Normalize();
+
+        animator.SetFloat("MoveX", input.x);
+        animator.SetFloat("MoveY", input.y);
+        animator.SetFloat("MoveMagnitude", input.magnitude);
+        animator.SetFloat("LastMoveX", lastMoveDirection.x);
+        animator.SetFloat("LastMoveY", lastMoveDirection.y);
     }
 
     void FixedUpdate() {
-        rigidbody2D.linearVelocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        rigidbody2D.linearVelocity = input * runSpeed;
     }
 
 }
